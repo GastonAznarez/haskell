0c673a153cf6ca0e5c2810014751a2ca5ab2029f
Gaston Aznarez <gastonaznarez@gmail.com>
2017-03-22 10:44:46 -0300

    Actividad 16

--- uno.hs
+++ uno.hs
@@ -1,3 +1,6 @@
+
+--Trabajo en clase 19/03
+
 f x = 5 * x
 
 duplica a = a + a
@@ -10,3 +13,22 @@ signo::Int -> Int
 signo x | x == 0 = 0
         | x > 1 = 1
         | x < 1 = -1
+
+--Actividad 16
+
+entre0y9::Int -> Bool
+entre0y9 x = x<10 && x>(-1)
+
+rangoPrecio::Int -> String
+rangoPrecio x   | x<0 = "esto no puede ser!"
+                | x<2000 = "muy barato"
+                | x>5000 = "demaciado caro"
+                | x>2000 && x<5000 = "hay que venderlo"
+
+absoluto::Int -> Int
+absoluto x   | x<0 = (-x)
+             | x>0 = x
+             | x == 0 = 0
+
+esMultiplo2:: Int -> Bool
+esMultiplo2 x = mod x 2 == 0
