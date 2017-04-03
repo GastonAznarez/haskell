33043d7d5b0b822934bb35e1881d95eb7a5207bd
Gaston Aznarez <gastonaznarez@gmail.com>
2017-04-03 17:53:30 -0300

    Dos.hs

--- dos.hs
+++ dos.hs
@@ -5,17 +5,17 @@
 soloPares :: [Int] -> [Int]
 soloPares [] = []
 soloPares (x:xs) | mod x 2 == 0 = x:soloPares xs
-		 							| mod x 2 /=0 = soloPares xs
+		 | mod x 2 /=0 = soloPares xs
 
 mayoresQue10 :: [Int] -> [Int]
 mayoresQue10 [] = []
 mayoresQue10 (x:xs) | x > 10 = x:mayoresQue10 xs
-		    						| x <= 10 = mayoresQue10 xs
+		    | x <= 10 = mayoresQue10 xs
 
 mayoresQue :: Int -> [Int] -> [Int]
 mayoresQue y [] = []
 mayoresQue y (x:xs) | x > y = x:mayoresQue y xs
-		    						| x <= y = mayoresQue y xs
+		    | x <= y = mayoresQue y xs
 
 --Actividad 4
 
@@ -31,9 +31,55 @@ multiplica :: Int -> [Int] -> [Int]
 multiplica y [] = []
 multiplica y (x:xs) = (x*y):multiplica y xs
 
+
 --Actividad 5
 
 todosMenores10 :: [Int] -> Bool
 todosMenores10 [] = True
 todosMenores10 (x:xs) | x >= 10 = False
-							 				| x:todosMenores10 xs
+                      | x < 10 = todosMenores10 xs
+
+hay0 :: [Int] -> Bool
+hay0 [] = False
+hay0 (x:xs) | x == 0 = True
+            | x /= 0 = hay0 xs
+
+suma :: [Int] -> Int
+suma [] = 0
+suma (x:xs) = x + (sum xs)
+
+
+
+--Actividad 9
+
+maximo :: [Int] -> Int
+maximo [x] = x
+maximo (x:(y:xs)) | x >= y = maximo (x:xs)
+                  | x < y = maximo (y:xs) 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
