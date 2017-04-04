b1a97dc3b34b78b496a086a4cf5472083e66db26
Gaston Aznarez <gastonaznarez@gmail.com>
2017-04-03 23:11:46 -0300

    3/4 y directores

--- dos.hs
+++ dos.hs
@@ -57,16 +57,25 @@ maximo [x] = x
 maximo (x:(y:xs)) | x >= y = maximo (x:xs)
                   | x < y = maximo (y:xs) 
 
+sumaPares :: [(Int,Int)] -> Int
+sumaPares [] = 0
+sumaPares ((x,y):xs) = (x + y) + (sumaPares xs)
+
+todos0y1 :: [Int] -> Bool
+todos0y1 [] = True
+todos0y1 (x:xs) | x == 1 = todos0y1 xs
+                | x == 0 = todos0y1 xs
+                | not (x == 0 || x == 1) = False
+
+quitar0s :: [Int] -> [Int]
+quitar0s [] = []
+quitar0s (x:xs) | x == 0 = quitar0s xs
+                | x /= 0 = x:(quitar0s xs)
 
 
-
-
-
-
-
-
-
-
+ultimo :: [a] -> a
+ultimo (x:xs) | length xs == 0 = x
+              | length xs /= 0 = ultimo xs 
 
 
 
